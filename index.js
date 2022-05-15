34088cd93080dcf270015c9ac26474d61ce5548c
stefancho <stefancho@joseongjins-MacBook-Pro.local>
2022-05-15 19:34:16 +0900

    init setting for eslint, prettier

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: [],
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {},
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  }
+}
