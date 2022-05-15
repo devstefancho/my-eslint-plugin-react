b2269b798839df1653edcbf70c63eb0a84aa3ec3
stefancho <stefancho@joseongjins-MacBook-Pro.local>
2022-05-15 22:48:12 +0900

    fix - change import style

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  extends: [],
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    "prettier/prettier": [
+      'error',
+      {
+        "printWidth": 80,
+        "singleQuote": true,
+        "trailingComma": "es5",
+        "tabWidth": 8
+      }
+    ]
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  }
+}
--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-module.exports = {
-  extends: [],
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-    node: true,
-    jest: true,
-  },
-  parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  rules: {
-    "prettier/prettier": [
-      'error',
-      {
-        "printWidth": 80,
-        "singleQuote": true,
-        "trailingComma": "es5",
-        "tabWidth": 4
-      }
-    ]
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  }
-}
+const eslintrc = require('./.eslintrc')
+
+module.exports = eslintrc
